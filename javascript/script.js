485388d8c5544857ef1f218e7f72b6fa022a346d
Shaireen <jankowskam401@gmail.com>
2020-05-03 17:35:45 +0200

    changed header image, added code credits

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
- function myFunction() {
+ function responsiveNav() {
   var x = document.querySelector(".nav-wrapper");
   if (x.className === "nav-wrapper") {
     x.className += " responsive";
@@ -6,3 +6,5 @@
     x.className = "nav-wrapper";
   }
 }
+
+// code inspired by https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
